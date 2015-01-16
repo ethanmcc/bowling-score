32b987e9043539e206061bbf9ff632191710fa9a
Ethan McCreadie <ethanmcc@gmail.com>
2015-01-16 10:50:23 -0500

    Week three changes

--- bowl.js
+++ bowl.js
@@ -1,12 +1,39 @@
+function sum(a, b) {
+    return a + b;
+}
+
 var game = function game(){
+    this.rolls = [];
+    this.total = 0;
 }
 
 game.prototype.roll = function(pins){
+    this.rolls.push(pins);
 }
 
 
-game.prototype.score = function(){
-	return -1;
+game.prototype.score = function(rolls, runningTotal, frame) {
+    var isSpare = function(rolls) { return rolls.slice(0, 2).reduce(sum) == 10 },
+        isStrike = function(rolls) { return rolls[0] == 10 };
+
+    if (typeof runningTotal == 'undefined') runningTotal = 0;
+    if (typeof rolls == 'undefined') rolls = this.rolls;
+    if (typeof frame == 'undefined') frame = 0;
+    frame += 1
+
+    if (rolls.length == 0 || frame > 10) {
+        return runningTotal;
+    } else if (isStrike(rolls)) {
+        runningTotal += rolls.slice(0, 3).reduce(sum);
+        return this.score(rolls.slice(1), runningTotal, frame);
+    } else {
+        if (isSpare(rolls)) {
+            runningTotal += rolls.slice(0, 3).reduce(sum);
+        } else {
+            runningTotal += rolls.slice(0, 2).reduce(sum);
+        }
+        return this.score(rolls.slice(2), runningTotal, frame);
+    }
 }
 
 if (typeof module != 'undefined') {
--- test/bowling_test.js
+++ test/bowling_test.js
@@ -6,19 +6,82 @@ if (typeof require != 'undefined'){
     var gameConstructor = game
 }
 
+function rollMany(game, pins, count) {
+    for (var i = 0; i < count; i++) {
+        game.roll(pins);
+    }
+}
+
+function assertScore(game, score) {
+    game.score().should.equal(score);
+}
+
 describe('Bowling Game', function(){
 
   beforeEach('Setup a new game', function(){
-    //this.game = new gameConstructor();
+    this.game = new gameConstructor();
     });
 
   describe('Gutter game', function(){
     it('Should have a zero score when no pins are knocked down', function(){
-    game = new gameConstructor();
-    for (var i = 0; i < 20; i++) game.roll(0);
-    game.score().should.equal(0);
+    rollMany(this.game, 0, 20);
+    assertScore(this.game, 0);
     })
   })
 
+  describe('Simple game', function() {
+      it('Should show 20 for all ones', function() {
+          rollMany(this.game, 20, 1);
+          assertScore(this.game, 20);
+      });
+  });
 
-})
+  describe('Mixed game', function() {
+      it('Should total 70', function() {
+          rollMany(this.game, 3, 10);
+          rollMany(this.game, 4, 10);
+          assertScore(this.game, 70);
+      });
+  });
+
+  describe('Three fives', function() {
+      it('Should be 20', function() {
+          rollMany(this.game, 5, 3);
+          rollMany(this.game, 0, 17);
+          assertScore(this.game, 20);
+      });
+  });
+
+  describe('Two spares', function() {
+      it('Should be 35', function() {
+          rollMany(this.game, 5, 5);
+          rollMany(this.game, 0, 15);
+          assertScore(this.game, 35);
+      });
+  });
+
+  describe('Single strike', function() {
+      it('Should be 22', function() {
+          rollMany(this.game, 10, 1);
+          rollMany(this.game, 3, 2);
+          rollMany(this.game, 0, 16);
+          assertScore(this.game, 22);
+      });
+  });
+
+  describe('Two strikes', function() {
+      it('Should be 16', function() {
+          rollMany(this.game, 10, 2);
+          rollMany(this.game, 3, 2);
+          rollMany(this.game, 0, 14);
+          assertScore(this.game, 45);
+      });
+  });
+
+  describe('Perfect game', function() {
+      it('Should be 300', function() {
+          rollMany(this.game, 10, 12);
+          assertScore(this.game, 300);
+      });
+  });
+});
